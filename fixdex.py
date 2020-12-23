b9841e0941cc9fb1ad3f23f97b4543fae0fea51f
larkpwn <ys-23@qq.com>
2020-12-23 16:17:46 +0800

    Update fixdex.py

--- fixdex.py
+++ fixdex.py
@@ -13,7 +13,7 @@ def fix():
 	except:
 		print("Usage: fixdex.py {.dex filename}")
 		return -1
-	
+
 	sapk.seek(0)	
 	w_open = sapk.read(8)
 
@@ -22,13 +22,13 @@ def fix():
 
 	sapk.seek(36)
 	w_other = sapk.read()
-	
+
 	buf1 = w_filesize + w_other 
 	w_signature = ba.a2b_hex(hashlib.sha1(buf1).hexdigest())
-	
+
 	buf2 = w_signature + buf1
 	w_checksum = zlib.adler32(buf2).to_bytes(length=4, byteorder='little')
-	
+
 	dapk.write(w_open)
 	dapk.write(w_checksum)
 	dapk.write(w_signature)
@@ -37,12 +37,12 @@ def fix():
 	dapk.close()
 	sapk.close()
 
-	
+
 	print("[+] magic \t\t%s" % w_open.decode().replace('\n',' '))
 	print("[+] checksum \t\t%s" % hex(int.from_bytes(w_checksum, byteorder='little')))
 	print("[+] signature \t\t%s" % ba.b2a_hex(w_signature).decode())
 	print("[+] filesize \t\t%u bytes" % int.from_bytes(w_filesize, byteorder='little'))
-	
+
 	return 0
 
 if __name__ == "__main__":
