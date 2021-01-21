d0d8ae418b1565ad300d2c320e2d9be0bbc2a66f
larkpwn <ys-23@qq.com>
2021-01-21 12:13:15 +0800

    Create hook_classes.js

---
+++ hook_classes.js
@@ -0,0 +1,51 @@
+Java.perform(function(){
+    
+    var q = "com.game*!*"; 
+    hook_classes(q);
+
+});
+
+//hook specify class
+function hook_class(cls_name)
+{
+	var cls = Java.use(cls_name);
+    var methods = cls.class.getDeclaredMethods();
+
+    //hook all method of the class
+    methods.forEach(function(method){
+
+        var method_name = method.getName(); //get method name
+
+        var overloads = cls[method_name].overloads;
+
+        overloads.forEach(
+        	function(ol){
+        		ol.implementation = function() {
+        			console.log('[+] ' + cls_name + ' > ' + method_name + '('+arguments.length+')');
+        			return this[method_name].apply(this, arguments);
+        		}
+        	}
+        );
+    });
+}
+// hook all classes matched query
+function hook_classes(q)
+{
+	var all = Java.enumerateMethods(q);
+
+	var cls_list = all[0]["classes"];
+	for (var i=0; i < cls_list.length; i++)
+	{
+		hook_class(cls_list[i]["name"]);
+	}
+}
+// hook query array
+function hook_a_classes(a)
+{
+	a.forEach(
+		function(query)
+		{
+			hook_classes(query);
+		}
+	);
+}
